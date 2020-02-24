900988d2b2584945f9f715029f9809cf4167b1a6
ImAuty <teraringu@gmail.com>
2020-02-24 17:17:38 +0900

    より分かりやすくなるように処理の流れを修正し、コメントを追加しました

--- ImageToText/MainWindow.xaml.cs
+++ ImageToText/MainWindow.xaml.cs
@@ -33,98 +33,74 @@ namespace ImageToText
         public MainWindow()
         {
             InitializeComponent();
-            this.Initialize();
-            WhiteStringTextBox.Text = WHITE_STRING;
-            BlackStringTextBox.Text = BLACK_STRING;
-            ThresholdTextBox.Text = THRESHOLD.ToString();
-            SeparatorTextBox.Text = SEPARATOR;
+            this.InitializeFileControls(FileOpenLabel, ImagesToTextButton, out this.filePaths);
+            this.SetTextControls(WhiteStringTextBox, BlackStringTextBox, ThresholdTextBox, SeparatorTextBox);
         }
 
-        private void Initialize()
-        {
-            this.filePaths = null;
-            FileOpenLabel.Content = "画像を選択してください";
-            ImageToTextButton.IsEnabled = false;
-        }
-
+        /// <summary>
+        /// FileOpenのボタンをクリックしたときの処理<br>
+        /// 画像選択ダイアログを表示して、選択された画像のパスを内部に保持する
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void FileOpenButton_Click(object sender, RoutedEventArgs e)
         {
             // ダイアログを設定する
-            var dialog = new OpenFileDialog { Filter = "Image Files(*.BMP;*.JPG;*.PNG;*.GIF)|*.BMP;*.JPG;*.PNG;*.GIF" };
-
+            var dialog = new OpenFileDialog();
+            // 画像選択となるようにフィルターを指定する
+            dialog.Filter = "Image Files(*.BMP;*.JPG;*.PNG;*.GIF)|*.BMP;*.JPG;*.PNG;*.GIF";
             // 複数選択を許可する
             dialog.Multiselect = true;
 
             // ファイル選択ダイアログを表示する
             var result = dialog.ShowDialog();
-
             if (!result.GetValueOrDefault())
-            { // ファイルが選択されない場合
-                this.Initialize();
+            {
+                // ファイルが選択されない場合
+                this.InitializeFileControls(FileOpenLabel, ImagesToTextButton, out this.filePaths);
                 return;
             }
 
-            // 選択されたファイルのパスを設定
+            // ファイルが選択された場合
+            // 選択されたファイルのパスを取得して内部に保持する
             this.filePaths = dialog.FileNames;
-
-            // ファイル名を取得する
+            // ファイル名を取得して表示する
             FileOpenLabel.Content = String.Join("\r\n", dialog.FileNames.Select(name => name.Split('\\').Last().Replace("_", "__")));
-            ImageToTextButton.IsEnabled = true;
+            ImagesToTextButton.IsEnabled = true;
             return;
         }
 
-        private void ImageToTextButton_Click(object sender, RoutedEventArgs e)
+        /// <summary>
+        /// ImagesToTextのボタンを押したときの処理<br>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ImagesToTextButton_Click(object sender, RoutedEventArgs e)
         {
             var fileNames = this.filePaths;
+
+            if (fileNames == null)
+            {
+                this.InitializeFileControls(FileOpenLabel, ImagesToTextButton, out this.filePaths);
+                return;
+            }
 
             foreach (var fileName in fileNames)
             {
                 if (!File.Exists(fileName))
                 { // ファイルが存在しない場合
-                    this.Initialize();
+                    this.InitializeFileControls(FileOpenLabel, ImagesToTextButton, out this.filePaths);
                     return;
                 }
             }
 
-            var whiteString = WHITE_STRING;
-            if (!String.IsNullOrEmpty(WhiteStringTextBox.Text))
-            {
-                whiteString = WhiteStringTextBox.Text;
-            }
-            else
-            {
-                WhiteStringTextBox.Text = WHITE_STRING;
-            }
-
-            var blackString = BLACK_STRING;
-            if (!String.IsNullOrEmpty(BlackStringTextBox.Text))
-            {
-                blackString = BlackStringTextBox.Text;
-            }
-            else
-            {
-                BlackStringTextBox.Text = BLACK_STRING;
-            }
-
-            var threshold = THRESHOLD;
-            if (!String.IsNullOrEmpty(ThresholdTextBox.Text) && Double.TryParse(ThresholdTextBox.Text, out double thresholdResult))
-            {
-                threshold = thresholdResult;
-            }
-            else
-            {
-                ThresholdTextBox.Text = THRESHOLD.ToString();
-            }
-
-            var separator = SEPARATOR;
-            if (!String.IsNullOrEmpty(SeparatorTextBox.Text))
-            {
-                separator = SeparatorTextBox.Text;
-            }
-            else
-            {
-                SeparatorTextBox.Text = SEPARATOR;
-            }
+            // 入力値を設定する
+            this.SetTextControls(WhiteStringTextBox, BlackStringTextBox, ThresholdTextBox, SeparatorTextBox);
+            var whiteString = WhiteStringTextBox.Text;
+            var blackString = BlackStringTextBox.Text;
+            var threshold = double.Parse(ThresholdTextBox.Text);
+            var separator = SeparatorTextBox.Text;
 
             foreach (var fileName in fileNames)
             {
@@ -134,7 +110,7 @@ namespace ImageToText
                     var bitmapString = Enumerable.Range(0, bitmap.Height).Select(j =>
                         Enumerable.Range(0, bitmap.Width).Select(i => BitmapUtility.ColorToSpecifiedString(bitmap.GetPixel(i, j), whiteString, blackString, threshold)));
 
-                    var windowText = String.Join("\r\n", bitmapString.Select(x => String.Join(separator, x)));
+                    var windowText = string.Join("\r\n", bitmapString.Select(x => string.Join(separator, x)));
 
                     var subWindow = new SubWindow(windowText)
                     {
@@ -142,6 +118,36 @@ namespace ImageToText
                     };
                     subWindow.Show();
                 }
+            }
+        }
+
+        private void InitializeFileControls(Label fileOpenLabel, Button imageToTextButton, out string[] filePaths)
+        {
+            fileOpenLabel.Content = "画像を選択してください";
+            imageToTextButton.IsEnabled = false;
+            filePaths = null;
+        }
+
+        private void SetTextControls(TextBox whiteStringTextBox, TextBox blackStringTextBox, TextBox thresholdTextBox, TextBox separatorTextBox)
+        {
+            if (string.IsNullOrEmpty(whiteStringTextBox.Text))
+            {
+                whiteStringTextBox.Text = WHITE_STRING;
+            }
+
+            if (string.IsNullOrEmpty(blackStringTextBox.Text))
+            {
+                blackStringTextBox.Text = BLACK_STRING;
+            }
+
+            if (string.IsNullOrEmpty(thresholdTextBox.Text) || !Double.TryParse(thresholdTextBox.Text, out _))
+            {
+                thresholdTextBox.Text = THRESHOLD.ToString();
+            }
+
+            if (string.IsNullOrEmpty(SeparatorTextBox.Text))
+            {
+                separatorTextBox.Text = SEPARATOR;
             }
         }
     }
